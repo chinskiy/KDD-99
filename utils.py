e5853f236bf88c3d6c91b0261e9f2434911331e4
chinskiy <chinskiy93@gmail.com>
2017-03-25 18:47:41 +0200

    add correlation

--- utils.py
+++ utils.py
@@ -13,6 +13,37 @@ names = ["duration", "protocol_type", "service", "flag", "src_bytes",
          "dst_host_srv_rerror_rate", "label"]
 
 categorical_names = ['protocol_type', 'service', 'flag']
+label_names = ['label', 'label_binary_num', 'label_four', 'label_four_num']
+
+label_to_four_attack_class = {'back.': 'DOS',
+                              'land.': 'DOS',
+                              'neptune.': 'DOS',
+                              'pod.': 'DOS',
+                              'smurf.': 'DOS',
+                              'teardrop.': 'DOS',
+                              'satan.': 'Probe',
+                              'ipsweep.': 'Probe',
+                              'nmap.': 'Probe',
+                              'portsweep.': 'Probe',
+                              'normal.': 'Normal',
+                              'guess_passwd.': 'R2L',
+                              'ftp_write.': 'R2L',
+                              'imap.': 'R2L',
+                              'phf.': 'R2L',
+                              'spy.': 'R2L',
+                              'multihop.': 'R2L',
+                              'warezmaster.': 'R2L',
+                              'warezclient.': 'R2L',
+                              'buffer_overflow.': 'U2R',
+                              'loadmodule.': 'U2R',
+                              'perl.': 'U2R',
+                              'rootkit.': 'U2R'}
+
+five_classes_to_num = {'Normal': 0,
+                       'Probe': 1,
+                       'R2L': 2,
+                       'U2R': 3,
+                       'DOS': 4}
 
 # Traffic features computed using a two-second time window
 traffic_features = ["count", "srv_count", "serror_rate", "srv_serror_rate",
