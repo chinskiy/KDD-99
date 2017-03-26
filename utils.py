3cfe0ad78c0cd6f5ffb51ca909fdf3b2848c48b6
chinskiy <chinskiy93@gmail.com>
2017-03-26 22:14:16 +0300

    add if train check

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 
-def model_validation(model, X_data_train, X_data_test, Y_data_train, Y_data_test):
+def model_validation(model, X_data_train, X_data_test, Y_data_train, Y_data_test, train=True):
     model.fit(X_data_train)
-    print('Train:')
-    print(accuracy_score(Y_data_train, model.predict(X_data_train)))
-    print(confusion_matrix(Y_data_train, model.predict(X_data_train)))
+    if train:
+        print('Train:')
+        print(accuracy_score(Y_data_train, model.predict(X_data_train)))
+        print(confusion_matrix(Y_data_train, model.predict(X_data_train)))
 
     print('Test:')
     print(accuracy_score(Y_data_test, model.predict(X_data_test)))
